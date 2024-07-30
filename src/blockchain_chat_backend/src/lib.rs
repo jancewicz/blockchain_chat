122ecb6bde17f8d892e852d70b278e0f18cbd000
jancewicz <jancewicz.pawel1@gmail.com>
2024-07-30 19:52:02 +0200

    cleaned code and optimized rust code

--- src/blockchain_chat_backend/src/lib.rs
+++ src/blockchain_chat_backend/src/lib.rs
@@ -5,36 +5,12 @@ use candid::Principal;
 use ic_cdk::caller;
 
 thread_local! {
-    static NOTES: RefCell<HashMap<Principal, Vec<String>>> = RefCell::default();
     static CHAT: RefCell<HashMap<[Principal; 2], Vec<String>>> = RefCell::default();
 }
 
 #[ic_cdk::query]
-fn get_chat(first_user: Principal, second_user: Principal) -> Option<Vec<String>> {
-    CHAT.with_borrow(|notes| notes.get(&[first_user, second_user]).cloned())
-}
-
-#[ic_cdk::query]
-fn get_notes(user: Principal) -> Option<Vec<String>> {
-    NOTES.with_borrow(|notes| notes.get(&user).cloned())
-}
-
-#[ic_cdk::update]
-fn add_note(note: String) {
-    let user = caller();
-    if user == Principal::anonymous() {
-        panic!("User is anonymous Principal");
-    }
-
-    NOTES.with_borrow_mut(|notes| {
-        let mut_notes = notes.get_mut(&user);
-
-        if let Some(notes_vec) = mut_notes {
-            notes_vec.push(note);
-        } else {
-            notes.insert(user, vec![note]);
-        }
-    })
+fn get_chat(chat_path: [Principal; 2]) -> Option<Vec<String>> {
+    CHAT.with_borrow(|notes| notes.get(&chat_path).cloned())
 }
 
 #[ic_cdk::update]
